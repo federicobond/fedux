3720ccfc277c7401c14b73021710713fce1ec6b2
Federico Bond <federicobond@gmail.com>
2012-05-29 14:44:18 -0300

    Added initial implementation of string.h and stdio.h libraries

---
+++ src/stdio.c
@@ -0,0 +1,37 @@
+#include "stdio.h"
+
+int
+putc(int ch, FILE *stream)
+{
+    char buf[1];
+    buf[0] = ch;
+    write(stream->fd, (void *)buf, 1);
+}
+
+int
+getc(FILE *stream)
+{
+    return 0;
+}
+
+int putchar(int ch)
+{
+    putc(ch, stdout);
+}
+
+int getchar(void)
+{
+    getc(stdin);
+}
+
+ssize_t
+read(int fd, void *buffer, size_t size)
+{
+    return 0;
+}
+
+ssize_t
+write(int fd, const void *buffer, size_t size)
+{
+    return 0;
+}
---
+++ src/string.h
@@ -0,0 +1,14 @@
+#ifndef _STRING_H
+#define _STRING_H
+
+#define NULL 0
+
+typedef int size_t;
+
+char   *strcpy(char *dest, const char *src);
+char   *strncpy(char *dest, const char *src, size_t n);
+size_t  strlen(const char *s);
+int     strcmp(const char *s1, const char *s2);
+int     strncmp(const char *s1, const char *s2, size_t n);
+
+#endif
