f6c34e2a96a5519ac9bb464265045f602da8def3
Federico Bond <federicobond@gmail.com>
2012-06-02 14:58:24 -0300

    Added more ctype.h functions and removed deprecated isascii

--- include/ctype.h
+++ include/ctype.h
@@ -8,8 +8,9 @@ int toupper(int c);
 int tolower(int c);
 int isalpha(int c);
 int isalnum(int c);
-int isascii(int c);
 int iscntrl(int c);
 int isspace(int c);
+int isprint(int c);
+int isxdigit(int c);
 
 #endif
