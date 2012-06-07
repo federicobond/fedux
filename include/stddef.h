f4ca498cd92301e059bf544fc2d3b98b3bb0d35b
Federico Bond <federicobond@gmail.com>
2012-06-06 23:19:33 -0300

    Improved stddef.h

--- include/stddef.h
+++ include/stddef.h
@@ -2,12 +2,12 @@
 #define _STDDEF_H
 
 #ifndef NULL
-#define NULL  0
+#define NULL (void *)0
 #endif
 
 typedef unsigned int size_t;
 typedef int ssize_t;
 
-typedef enum { false, true } bool;
+typedef enum { false = 0, true } bool;
 
 #endif
